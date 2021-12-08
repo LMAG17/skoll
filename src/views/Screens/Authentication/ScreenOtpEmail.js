56fcfaa32b29ed16da072115a056f369ca0989d5
Luis Miguel Alvarez Gil <luismiguelalvarez46@gmail.com>
2021-12-08 12:52:51 -0500

    improving validate otp logic

--- src/views/Screens/Authentication/ScreenOtpEmail.js
+++ src/views/Screens/Authentication/ScreenOtpEmail.js
@@ -1,45 +1,42 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-import { generateOtp, validateOtp as validateOtpService } from '../../../services/ServiceInteractor';
 import InputCustom from '../../../components/InputCustom';
+import { generateOtp, validateOtp as validateOtpService } from '../../../services/ServiceInteractor';
 import { WidthDP } from '../../../utils/CalculateSize';
 
 const ScreenOtpEmail = (props) => {
-    const [data, setData] = useState({});
-    const [token, setToken] = useState({});
-    const dispatch = useDispatch();
-    const serviceResponse = useSelector(state => state.serviceResponse);
-    const serviceOtp = useSelector(state => state.serviceOtp);
-    const validateOtp = useSelector(state => state.validateOtp);
-    const sessionId = useSelector(state => state.sessionId);
 
+    const [data, setData] = useState({});
+
+    const dispatch = useDispatch();
+    
+    const sessionId = useSelector(state => state.sessionId);
 
     const handleGenerateOtp = async () => {
         console.log("FUncion de la vista", sessionId);
-        const body = { "sessionId": sessionId }
         try {
-            let responseGenerateOtp = await generateOtp({ transaction: "GENERATE_OTP_REGISTRY", body });
+            let responseGenerateOtp = await generateOtp({ transaction: "GENERATE_OTP_REGISTRY", sessionId });
             console.log("RESPUESTA_Otp", responseGenerateOtp);
         } catch (error) {
             console.log("ERROR DE VISTA", error);
         }
     }
+
     const handleValidateOtp = async () => {
         try {
-            let responseValidateOtp = await validateOtpService("VALIDATE_OTP_REGISTRY", data)
+            let responseValidateOtp = await validateOtpService({ transaction: "VALIDATE_OTP_REGISTRY", sessionId, otp: data.otp })
             console.log("RESPUESTA_ValidateOtp", responseValidateOtp);
             props.navigation.navigate('FormRegister')
         } catch (error) {
             console.log("ERROR DE VISTA", error);
         }
     }
+
     const handleChange = (key, value) => {
         setData({
             [key]: value,
-            sessionId
         })
-
     }
     return (
         <View style={styles.container}>
